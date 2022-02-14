ea08328c42be47eb6dd69dc4b3f3188367aaff9b
IyanXyz <99070914+IyanXyz@users.noreply.github.com>
2022-02-14 13:05:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,16 +9,17 @@
 // thank you to fernazer
 // thank you to MikeBot Dev Team
 // thank you to ALL Bot creator
+// thank you to pilarv2
 // and thanks you to who support my Bot
 
-gc1 = ''
+gc1 = 'https://tinyurl.com/yapnjvdt'
 gc2 = ''
 gc3 = ''
-global.linkGC = ['', ''] // ganti jadi group lu
-global.owner = ['6289625556161','62896255561613'] // Put your number here //owner eval
-global.kontak = ['6289625556161','62896255561613'] //Ketika ada yang ngetik #owner
+global.linkGC = ['https://tinyurl.com/yapnjvdt', ''] // ganti jadi group lu
+global.owner = ['6285742344873', '6281244706456', '6281214769844', '6281913868660'] // Put your number here //owner eval
+global.kontak = ['6285742344873', '6281244706456', '6281214769844', '6281913868660'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6289625556161','62896255561613'] // Premium user has unlimited limit
+global.prems = ['6285742344873', '6281244706456', '6281214769844', '6281913868660'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   rey: 'https://server-api-rey.herokuapp.com',
@@ -49,25 +50,25 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'https://github.com/IyanXyz'
 kasihcaption = `Nih Om😎`
-namakontak1 = 'Pilar Owner Bot'
-namakontak2 = 'Caca Admin Bot'
+namakontak1 = 'IyanXyz'
+namakontak2 = 'Kimiko'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'Zeus Botz' // ganti aja
-global.author = 'Zeus Botz' // ganti aja
+global.packname = 'GuraBot' // ganti aja
+global.author = 'Gura Punya Saya' // ganti aja
 
 //yyy
-bc = 'ZeusBotz' //ZeusBotz Broadcast
-footer = '\n© ZeusBotz'
-namabot = 'ZEUS BOTZ'
-namalu = 'PILAR'
-watermark = 'ZeusBotz' //ganti aja
+bc = 'Gura Bot' //Broadcast Watermak
+footer = 'Gura Bot The Best!!!'
+namabot = 'Gura Bot'
+namalu = 'IyanXyz Ganzz'
+watermark = 'Gura Botzz, By IyanXyz' //ganti aja
 
 // 
 wait = '_*Tunggu Sebentar...*_'
-global.botwm = 'ZeusBotz'
+global.botwm = 'Guraa Botz By IyanXyz'
 global.wait = '_*Tunggu Sebentar...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
@@ -76,11 +77,11 @@ global.eror = '_*Server Error*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23101820&text='
 
 // Ubah saja Tambahan By IyanXyz Ganz
-global.image = 'https://telegra.ph/file/f96eee5c2c75bd32c1f58.png'//thumbnail
-global.iyanxyz = 'https://telegra.ph/file/3338e1a48968776b43aa2.jpg'//thumbnail buat menu.js
+global.image = 'https://telegra.ph/file/3338e1a48968776b43aa2.jpg'//thumbnail
+global.iyanxyz = 'https://telegra.ph/file/3338e1a48968776b43aa2.jpg'//thumbnail buat menu
 
 // tingkat kesulitan, semakin tinggi semakin susah
-global.multiplier = 40 // The higher, The harder levelup
+global.multiplier = 150 // The higher, The harder levelup
 
 //*****************PEMBATAS*********************
 // JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
