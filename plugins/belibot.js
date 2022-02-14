c625a9729fc598487ea9f2f7ad28ddf6c64e3ce6
IyanXyz <99070914+IyanXyz@users.noreply.github.com>
2022-02-14 13:18:06 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -2,17 +2,15 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭═══ *〘 SEWA 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ Sewa = _5k/grup (1 minggu)_
-║┊ ⌲ Sewa = _15k/grup (1 bulan)_
-║┊ ⌲ Sewa = _20k/grup (2 bulan)_
-║┊ ⌲ Sewa = _35k/grup (8 bulan)_                                                                      ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
+║┊ ⌲ Sewa = _5k/grup (1 Bulan)_
+║┊ ⌲ Sewa = _15k/grup (Permanent)_                                                                      ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
 ╰═ ┅ ═══════
 ╭═══ *〘 BELI PREMIUM 〙*
 ║ ┅ ๑————————————๑
-║┊ ⌲ Premium = _10k/minggu_
-║┊ ⌲ Premium = _35k/bulan_
-║┊ ⌲ Premium = _46k/permanen
-║┊ ⌲ Jadi Owner = _75k/permanen
+║┊ ⌲ Premium = _10k/bulan_
+║┊ ⌲ Premium = _15k/permanent_
+║┊ ⌲ Jadi Owner = _25k/permanen
+║┊ ⌲ Black Card Vip = _30k/permanen
 ╰═ ┅ ═══════
 
 ༅ _*KEUNTUNGAN SEWABOT*_
@@ -27,11 +25,9 @@ let handler = async (m, { conn }) => {
 *[❗] Jika Group Sepi 2 minggu, bot akan leave*
 
 ◪ *Jika Minat Hubungi owner kami*
-◪ *Atau Masuk Group Official Bot*
-_https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y_
 
 ▌│█║▌║▌║║▌║▌║█│▌
-`.trim(), `ZeusBotz`, 'Payment', '#payment', 'Owner', '#owner', m)
+`.trim(), `GuraaBotz`, 'List Menu', '#menu', 'Owner', '#owner', m)
 }
 
 handler.help = ['sewa', 'sewabot']
