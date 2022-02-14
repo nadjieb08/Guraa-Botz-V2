dfebf80dfe715c0418712af0bac22d2dc4cbbd73
IyanXyz <99070914+IyanXyz@users.noreply.github.com>
2022-02-14 13:31:30 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA TRI3 : 6289625556161
-├ OVO & DANA : 6289625556161
+├ PULSA TRI3 : 085742344873
+├ GOPAY : 085742344873
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
